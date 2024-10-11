412e4f4772f019ed2eb95c377639911460855be7
Lynjai Jimenez <intermediadesignsllc@gmail.com>
2024-10-11 12:10:27 -0400

    Add global.css, global.d.ts, and .gitattributes files

---
+++ app/index.tsx
@@ -0,0 +1,13 @@
+import { Text, View } from 'react-native'
+
+export default function Index () {
+  return (
+    <View>
+      <div className='flex items-center justify-center h-screen'>
+        <Text className='text-center text-4xl'>
+          Edit app/index.tsx to edit this screen.
+        </Text>
+      </div>
+    </View>
+  )
+}
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // NOTE: Update this to include the paths to all of your component files.
+  darkMode: 'class',
+  content: [
+    './app/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './node_modules/@rnr/**/*.{ts,tsx}',
+  ],
+  presets: [require('nativewind/preset')],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
